7bdcc5260e4722392a6e429cd7e318e8efc6bfed
Cristiane Araujo Souza Dos Reis <c.souza.dos.reis@avanade.com>
2023-09-30 16:10:14 -0300

    melhoranda a escrita do teste

--- TreinamentoCypress/cypress/e2e/spec.cy.js
+++ TreinamentoCypress/cypress/e2e/spec.cy.js
@@ -1,43 +1,23 @@
-
 describe('Cenários de Testes de Login', () => {
+  const url = 'https://opensource-demo.orangehrmlive.com';
+  const usernameSelector = ':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input';
+  const passwordSelector = ':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input';
 
-  beforeEach(function(){
-
-    cy.visit('https://opensource-demo.orangehrmlive.com')
-
-  })
+  beforeEach(() => {
+    cy.visit(url);
+    cy.wait(10000);
+  });
 
   it('Falha no Login', () => {
+    cy.get(usernameSelector).type('Admins');
+    cy.get(passwordSelector).type('admin1234');
+    cy.contains('button', 'Login').click();
+  });
 
-    cy.wait(10000)
-
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admins')
-
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin1234')    
-
-    cy.contains('button', 'Login').click()
-
-  })
- 
   it('Login realizado com sucesso', () => {
-
-    cy.wait(10000)
-
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')    
-
-    cy.contains('button', 'Login').click()
-
-  })
-  
-})
-
-
-
-
-
-
-
- 
+    cy.get(usernameSelector).type('Admin');
+    cy.get(passwordSelector).type('admin123');
+    cy.contains('button', 'Login').click();
+  });
+});
 
