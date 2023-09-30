2d2ccc2fd94c7e5711a4bb1dbbaaec761203fd5f
Cristiane Araujo Souza Dos Reis <c.souza.dos.reis@avanade.com>
2023-09-30 15:19:18 -0300

    TreinamentoCypress

---
+++ TreinamentoCypress/cypress/e2e/spec.cy.js
@@ -0,0 +1,52 @@
+
+describe('Cenários de Testes de Login', () => {
+
+  beforeEach(function(){
+
+    cy.visit('https://opensource-demo.orangehrmlive.com')
+
+  })
+
+  it('Falha no Login', () => {
+
+    cy.wait(10000)
+
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admins')
+
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin1234')    
+
+    cy.contains('button', 'Login').click()
+
+  })
+ 
+  it('Login realizado com sucesso', () => {
+
+    cy.wait(10000)
+
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')    
+
+    cy.contains('button', 'Login').click()
+
+  })
+
+  it('',() => {
+
+  })
+  it('',() => {
+
+  })
+
+
+  
+})
+
+
+
+
+
+
+
+ 
+
